34803d7fcbc1f1e1826f9594758e1e0103ad6350
Krok <kadetkroksapsp2@gmail.com>
2023-08-10 14:20:39 +0200

    dependencies

--- app/src/main/java/com/example/babysdiet/ui/theme/Color.kt
+++ app/src/main/java/com/example/babysdiet/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val ExcellentEvaluationColor = Color(0xFF00FF00)
+val FineEvaluationColor = Color(0xFF66FF33)
+val NeutralEvaluationColor = Color(0xFFFFFF00)
+val BadEvaluationColor = Color(0xFFFF6600)
+val VeryBadEvaluationColor = Color(0xFFFF0000)
+//val MediumPriorityColor = Color(0XFFFFC114)
+//val HighPriorityColor = Color(0xFFFF4646)
+//val NonePriorityColor = MediumGrey
