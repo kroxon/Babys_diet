dde75d93f8189389575bfd3fead31cabd5ccf7e3
Krok <kadetkroksapsp2@gmail.com>
2023-08-25 20:14:05 +0200

    repair

---
+++ app/src/main/java/com/example/babysdiet/navigation/destinations/homeComposable.kt
@@ -0,0 +1,32 @@
+package com.example.babysdiet.navigation.destinations
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.babysdiet.ui.screens.home.HomeScreen
+import com.example.babysdiet.ui.viewmodels.SharedViewModel
+import com.example.babysdiet.util.Constants.HOME_ARGUMENT_KEY
+import com.example.babysdiet.util.Constants.HOME_SCREEN
+
+
+fun NavGraphBuilder.homeComposable(
+    navigateToCategoryScreen: (categoryId: Int) -> Unit,
+    navigateToDiaryScreen: (diaryId: Int) -> Unit,
+    sharedViewModel: SharedViewModel
+) {
+    composable(
+        route = HOME_SCREEN,
+        arguments = listOf(navArgument(HOME_ARGUMENT_KEY) {
+            type = NavType.StringType
+        })
+    ) {
+        HomeScreen(
+            navigateToCategoryScreen = {},
+            navigateToDiaryScreen = {}
+            ,
+            sharedViewModel = sharedViewModel
+        )
+    }
+}
+
