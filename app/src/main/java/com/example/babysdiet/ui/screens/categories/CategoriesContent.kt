2e588546b18b2fd85fd8fbeb80b3c1ba977424d7
Krok <kadetkroksapsp2@gmail.com>
2023-09-23 22:26:05 +0200

    home screen

--- app/src/main/java/com/example/babysdiet/ui/screens/categories/CategoriesContent.kt
+++ app/src/main/java/com/example/babysdiet/ui/screens/categories/CategoriesContent.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
@@ -38,7 +40,9 @@ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.babysdiet.R
+import com.example.babysdiet.components.ProductAdvanceItem
 import com.example.babysdiet.components.ProductItem
+import com.example.babysdiet.components.data.models.Diary
 import com.example.babysdiet.components.data.models.Product
 import com.example.babysdiet.ui.theme.LARGE_PADDING
 import com.example.babysdiet.ui.theme.SMALL_PADDING
@@ -49,157 +53,149 @@ import com.example.babysdiet.util.RequestState
 @Composable
 fun CategoriesContent(
     selectedProducts: RequestState<List<Product>>,
+    allDiaries: RequestState<List<Diary>>,
     onProductSelected: (Product) -> Unit,
     selectedCategoryId: Int,
     selectedProductId: Int
 ) {
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .padding(LARGE_PADDING)
-    ) {
-        Spacer(modifier = Modifier.padding(top = TOP_APP_BAR_HEIGHT))
-
-        SearchableExposedDropdownMenuBox(
-            allProductsRequest = selectedProducts,
-            onProductSelected = onProductSelected
-        )
-
-        Column(Modifier.fillMaxWidth()) {
-            Text(
-                text = selectedCategoryId.toString(),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(SMALL_PADDING)
-            )
-            Text(
-                text = selectedProductId.toString(),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(SMALL_PADDING)
-            )
-        }
-    }
-}
-
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
-@Composable
-fun SearchableExposedDropdownMenuBox(
-    allProductsRequest: RequestState<List<Product>>,
-    onProductSelected: (Product) -> Unit
-) {
     var allProducts: List<Product> = emptyList()
-    if (allProductsRequest is RequestState.Success)
-        allProducts = allProductsRequest.data
-    val context = LocalContext.current
-    var expanded by remember { mutableStateOf(false) }
-    var selectedText by remember { mutableStateOf("") }
-    val keyboardController = LocalSoftwareKeyboardController.current
-    val focusManager = LocalFocusManager.current
-    var isClearButtonVisible by remember { mutableStateOf(false) }
+    if (selectedProducts is RequestState.Success)
+        allProducts = selectedProducts.data
+    var diaries: List<Diary> = emptyList()
+    if (allDiaries is RequestState.Success)
+        diaries = allDiaries.data
 
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = SMALL_PADDING, end = SMALL_PADDING, bottom = SMALL_PADDING)
+    Spacer(modifier = Modifier.padding(top = TOP_APP_BAR_HEIGHT))
+
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth()
     ) {
-        ExposedDropdownMenuBox(
-            modifier = Modifier.fillMaxWidth(),
-            expanded = expanded,
-            onExpandedChange = {
-                expanded = !expanded
-                if (!expanded) {
-                    keyboardController?.hide() // Hide the keyboard to remove focus
-                    focusManager.clearFocus()
-                    isClearButtonVisible = false
-                }
-            }
-        ) {
-            OutlinedTextField(
-                value = selectedText,
-                onValueChange = {
-                    selectedText = it
-                    isClearButtonVisible = it.isNotEmpty()
-                },
-                label = {
-                    Text(
-                        text = stringResource(id = R.string.search),
-                        modifier = Modifier.padding(start = SMALL_PADDING)
-                    )
-                },
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                modifier = Modifier
-                    .menuAnchor()
-                    .fillMaxWidth(),
-                shape = RoundedCornerShape(24.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = MaterialTheme.colorScheme.topAppBarBackgroumdColor,
-                    focusedTextColor = MaterialTheme.colorScheme.topAppBarBackgroumdColor,
-                    focusedLabelColor = MaterialTheme.colorScheme.topAppBarBackgroumdColor,
-                    unfocusedBorderColor = Color.DarkGray,
-                    unfocusedLabelColor = Color.DarkGray,
-                    unfocusedTextColor = Color.DarkGray
-                )
-            )
-            if (isClearButtonVisible) {
-                Box(
-                    modifier = Modifier
-                        .align(Alignment.CenterEnd)
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Clear,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .padding(end = 48.dp, top = SMALL_PADDING)
-                            .size(24.dp)
-                            .clickable {
-                                selectedText = ""
-                            },
-                        tint = Color.Gray,
-                    )
-                }
-            }
-
-            val filteredOptions =
-                allProducts.filter { it.name.contains(selectedText, ignoreCase = true) }
-            if (filteredOptions.isNotEmpty()) {
-                ExposedDropdownMenu(
-                    modifier = Modifier.fillMaxWidth(),
-                    expanded = expanded,
-                    onDismissRequest = {
-                        // We shouldn't hide the menu when the user enters/removes any character
-//                        keyboardController?.hide() // Hide the keyboard to remove focus
-//                        focusManager.clearFocus()
-                        isClearButtonVisible = false
-                    }
-                ) {
-                    filteredOptions.forEach { item ->
-                        DropdownMenuItem(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(36.dp),
-                            text = {
-                                item.name
-                            },
-                            leadingIcon = {
-                                ProductItem(product = item)
-                            },
-                            onClick = {
-                                onProductSelected(item)
-                                selectedText = ""
-                                expanded = false
-                                Toast.makeText(context, item.name, Toast.LENGTH_SHORT).show()
-                                keyboardController?.hide() // Hide the keyboard to remove focus
-                                focusManager.clearFocus()
-                                isClearButtonVisible = false
-                            }
-                        )
-                    }
-                }
-            }
+        items(allProducts) { product ->
+            ProductAdvanceItem(
+                product = product,
+                diaries = diaries.filter { it.productId == product.productId })
         }
     }
+
 }
+
+//@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+//@Composable
+//fun SearchableExposedDropdownMenuBox(
+//    allProductsRequest: RequestState<List<Product>>,
+//    onProductSelected: (Product) -> Unit
+//) {
+//    var allProducts: List<Product> = emptyList()
+//    if (allProductsRequest is RequestState.Success)
+//        allProducts = allProductsRequest.data
+//    val context = LocalContext.current
+//    var expanded by remember { mutableStateOf(false) }
+//    var selectedText by remember { mutableStateOf("") }
+//    val keyboardController = LocalSoftwareKeyboardController.current
+//    val focusManager = LocalFocusManager.current
+//    var isClearButtonVisible by remember { mutableStateOf(false) }
+//
+//    Box(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(start = SMALL_PADDING, end = SMALL_PADDING, bottom = SMALL_PADDING)
+//    ) {
+//        ExposedDropdownMenuBox(
+//            modifier = Modifier.fillMaxWidth(),
+//            expanded = expanded,
+//            onExpandedChange = {
+//                expanded = !expanded
+//                if (!expanded) {
+//                    keyboardController?.hide() // Hide the keyboard to remove focus
+//                    focusManager.clearFocus()
+//                    isClearButtonVisible = false
+//                }
+//            }
+//        ) {
+//            OutlinedTextField(
+//                value = selectedText,
+//                onValueChange = {
+//                    selectedText = it
+//                    isClearButtonVisible = it.isNotEmpty()
+//                },
+//                label = {
+//                    Text(
+//                        text = stringResource(id = R.string.search),
+//                        modifier = Modifier.padding(start = SMALL_PADDING)
+//                    )
+//                },
+//                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+//                modifier = Modifier
+//                    .menuAnchor()
+//                    .fillMaxWidth(),
+//                shape = RoundedCornerShape(24.dp),
+//                colors = OutlinedTextFieldDefaults.colors(
+//                    focusedBorderColor = MaterialTheme.colorScheme.topAppBarBackgroumdColor,
+//                    focusedTextColor = MaterialTheme.colorScheme.topAppBarBackgroumdColor,
+//                    focusedLabelColor = MaterialTheme.colorScheme.topAppBarBackgroumdColor,
+//                    unfocusedBorderColor = Color.DarkGray,
+//                    unfocusedLabelColor = Color.DarkGray,
+//                    unfocusedTextColor = Color.DarkGray
+//                )
+//            )
+//            if (isClearButtonVisible) {
+//                Box(
+//                    modifier = Modifier
+//                        .align(Alignment.CenterEnd)
+//                ) {
+//                    Icon(
+//                        imageVector = Icons.Default.Clear,
+//                        contentDescription = null,
+//                        modifier = Modifier
+//                            .padding(end = 48.dp, top = SMALL_PADDING)
+//                            .size(24.dp)
+//                            .clickable {
+//                                selectedText = ""
+//                            },
+//                        tint = Color.Gray,
+//                    )
+//                }
+//            }
+//
+//            val filteredOptions =
+//                allProducts.filter { it.name.contains(selectedText, ignoreCase = true) }
+//            if (filteredOptions.isNotEmpty()) {
+//                ExposedDropdownMenu(
+//                    modifier = Modifier.fillMaxWidth(),
+//                    expanded = expanded,
+//                    onDismissRequest = {
+//                        // We shouldn't hide the menu when the user enters/removes any character
+////                        keyboardController?.hide() // Hide the keyboard to remove focus
+////                        focusManager.clearFocus()
+//                        isClearButtonVisible = false
+//                    }
+//                ) {
+//                    filteredOptions.forEach { item ->
+//                        DropdownMenuItem(
+//                            modifier = Modifier
+//                                .fillMaxWidth()
+//                                .height(36.dp),
+//                            text = {
+//                                item.name
+//                            },
+//                            leadingIcon = {
+//                                ProductItem(product = item)
+//                            },
+//                            onClick = {
+//                                onProductSelected(item)
+//                                selectedText = ""
+//                                expanded = false
+//                                Toast.makeText(context, item.name, Toast.LENGTH_SHORT).show()
+//                                keyboardController?.hide() // Hide the keyboard to remove focus
+//                                focusManager.clearFocus()
+//                                isClearButtonVisible = false
+//                            }
+//                        )
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
